de2ea592801fb2884c0c38d2b81d2e2d162bd1f8
xuzhiwei <xuzhiwei4347@qq.com>
2020-05-31 17:48:07 +0800

    轮播图

---
+++ src/components/Home/Swiper.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOptions">
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl" alt="">
+            </swiper-slide>
+            <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeSwiper",
+        data(){
+            return {
+                swiperOptions:{
+                    pagination:{
+                        el:'.swiper-pagination'
+                    },
+                    loop:true,
+                    autoplay:true
+                },
+                swiperList:[{
+                    id:'0001',
+                    imgUrl:'https://imgs.qunarzz.com/vc/e3/a1/71/f498dfd3bed948d623c9093252.jpg_92.jpg'
+                },{
+                    id:'0002',
+                    imgUrl:'https://imgs.qunarzz.com/vc/44/e9/86/95bc36c9e1c06ebd68bdfe222e.jpg_92.jpg'
+                },{
+                    id:'0003',
+                    imgUrl:'https://imgs.qunarzz.com/vc/6d/9f/35/b8ad5468f73fd60ec0ced086f6.jpg_92.jpg'
+                }]
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper
+        overflow hidden
+        width 100%
+        height 0
+        padding-bottom 30.4%
+        background #eee
+        .swiper-img
+            width 100%
+    .wrapper >>> .swiper-pagination-bullet-active
+        background :#fff
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
   import HomeHeader from '../components/Home/Header'
+  import HomeSwiper from '../components/Home/Swiper'
 export default {
   name: 'Home',
   components:{
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
