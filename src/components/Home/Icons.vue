37b4e6e73ecdf9f2be7800ef487ab7a332b327ba
xuzhiwei <xuzhiwei4347@qq.com>
2020-06-01 17:51:41 +0800

    首页图标

---
+++ src/components/Home/Icons.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="icons">
+        <swiper>
+            <swiper-slide v-for="(page,index) of pages" :key="index">
+                <div class="icon" v-for="item of page" :key="item.id">
+                    <div class="icon-img">
+                        <img class="icon-img-content"
+                             :src="item.imgUrl"
+                             alt="">
+                    </div>
+                    <p class="icon-desc">{{item.desc}}</p>
+                </div>
+            </swiper-slide>
+        </swiper>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeIcons",
+        data(){
+            return {
+                iconList:[{
+                    id:'0001',
+                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+                    desc:'景点门票'
+                },{
+                    id:'0002',
+                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/fa/2548667cb6e902.png',
+                    desc:'东方明珠'
+                },{
+                    id:'0003',
+                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/de/f26be47a6bf02a02.png',
+                    desc:'上海迪士尼'
+                },{
+                    id:'0004',
+                    imgUrl:'https://img1.qunarzz.com/piao/fusion/1810/50/26ffa31b56646402.png',
+                    desc:'上海海昌'
+                },{
+                    id:'0005',
+                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+                    desc:'上海野生动物园'
+                },{
+                    id:'0006',
+                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/b1/528a9e80403b8c02.png',
+                    desc:'上海欢乐谷'
+                },{
+                    id:'0007',
+                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/8c/47630407f70e8302.png',
+                    desc:'上影乐园'
+                },{
+                    id:'0008',
+                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+                    desc:'上海杜莎'
+                },{
+                    id:'0009',
+                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+                    desc:'东方绿舟'
+                }]
+            }
+        },
+        computed:{
+            pages(){
+                const pages=[]
+                this.iconList.forEach((item,index)=>{
+                    const page=Math.floor(index/8)
+                    if(!pages[page]){
+                        pages[page]=[]
+                    }
+                    pages[page].push(item)
+                })
+                return pages
+            }
+        }
+    }
+</script>
+
+<style scoped lang="stylus">
+    @import "~@/assets/styles/varibles.styl"
+    @import "~@/assets/styles/mixins.styl"
+    .icons >>> .swiper-container
+        height 0
+        padding-bottom 50%
+    .icon
+        position relative
+        overflow hidden
+        float left
+        width 25%
+        height 0
+        padding-bottom 25%
+        .icon-img
+            position absolute
+            top 0
+            left 0
+            right 0
+            bottom .44rem
+            box-sizing border-box
+            padding .1rem
+            .icon-img-content
+                display block
+                margin 0 auto
+                height 100%
+        .icon-desc
+            position absolute
+            left 0
+            right 0
+            bottom 0
+            height .44rem
+            line-height .44rem
+            text-align center
+            color $darkTextColor
+            ellipsis()
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
   import HomeHeader from '../components/Home/Header'
   import HomeSwiper from '../components/Home/Swiper'
+  import HomeIcons from '../components/Home/Icons'
 export default {
   name: 'Home',
   components:{
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
