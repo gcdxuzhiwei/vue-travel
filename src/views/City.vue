4b9804943865ea7e172006a192aee8f22a1a321a
xuzhiwei <xuzhiwei4347@qq.com>
2020-06-01 23:54:21 +0800

    城市选择betterscroll

--- src/views/City.vue
+++ src/views/City.vue
@@ -2,15 +2,21 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list></city-list>
+        <city-alphabet></city-alphabet>
     </div>
 </template>
 
 <script>
     import CityHeader from '../components/City/Header'
     import CitySearch from "../components/City/Search";
+    import CityList from "../components/City/List";
+    import CityAlphabet from "../components/City/Alphabet";
     export default {
         name: "City",
         components:{
+            CityAlphabet,
+            CityList,
             CitySearch,
             CityHeader
         }
