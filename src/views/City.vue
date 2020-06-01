d4160e294752a3b640d911c98780bba5ca165f4b
xuzhiwei <xuzhiwei4347@qq.com>
2020-06-01 23:06:33 +0800

    城市搜索框

--- src/views/City.vue
+++ src/views/City.vue
@@ -1,12 +1,17 @@
 <template>
-    <city-header></city-header>
+    <div>
+        <city-header></city-header>
+        <city-search></city-search>
+    </div>
 </template>
 
 <script>
     import CityHeader from '../components/City/Header'
+    import CitySearch from "../components/City/Search";
     export default {
         name: "City",
         components:{
+            CitySearch,
             CityHeader
         }
     }
