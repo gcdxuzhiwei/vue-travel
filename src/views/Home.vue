5c79d2b8b2cd379efb916197aa0b6c0a35579272
xuzhiwei <xuzhiwei4347@qq.com>
2020-06-02 20:48:07 +0800

    finish page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,6 +15,7 @@
   import HomeRecommend from '../components/Home/Recommend'
   import HomeWeekend from '../components/Home/Weekend'
   import axios from 'axios'
+  import {mapState}from 'vuex'
 export default {
   name: 'Home',
   components:{
@@ -26,15 +27,19 @@ export default {
   },
   data(){
     return {
+      lastCity:'',
       swiperList:[],
       iconList:[],
       recommendList:[],
       weekendList:[]
     }
   },
+  computed:{
+    ...mapState(['city'])
+  },
   methods:{
     getHomeInfo(){
-      axios.get('/api/index.json')
+      axios.get('/api/index.json?city='+this.city)
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc(res){
@@ -49,7 +54,14 @@ export default {
     }
   },
   mounted() {
+    this.lastCity=this.city
     this.getHomeInfo()
+  },
+  activated() {
+    if (this.lastCity !== this.city){
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
